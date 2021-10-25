667d36f8c2103ad9f588a20a9cd77ae4751e6201
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2021-10-25 16:06:27 +0200

    fix usage with clipboard buffer, not just selection buffeR

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
     var toInject = 
        // Hurr durr javascript
 '(' + function() {
+    if (window.location.hostname.indexOf("mail.google.com") == -1) { // lgtm [js/incomplete-url-substring-sanitization]
+        return;
+    }
     function setGetSet(obj, propertyName, getFunc, setFunc) {
         try {
             Object.defineProperty(obj, propertyName, { set: setFunc, get: getFunc });
@@ -15,60 +18,65 @@
         node.innerHTML = str;
         document.body.appendChild(node);
     }
+    AnalyserNode.prototype.getByteTimeDomainData = function() {
+    }
 
     // Neuter the dumb confidential shit, I like to select the text I read
     if (window.location.hostname.indexOf("mail.google.com") != -1) { // lgtm [js/incomplete-url-substring-sanitization]
-        const orig_getSelection = window.getSelection;
-        setGetSet(window, 'getSelection',
-            () => function() {
-                var sel = orig_getSelection();
-                if (!sel.anchorNode) {
-                    return sel;
-                }
-                if (sel.anchorNode.parentElement.contentEditable) {
-                    return sel;
-                }
-
-                console.log("tried window getselection");
-                //console.trace();
-                return {
-                    'addRange': function() {},
-                    'anchorNode': null,
-                    'anchorOffset': 0,
-                    'baseNode': null,
-                    'baseOffset': 0,
-                    'extentNode': null,
-                    'extentOffset': 0,
-                    'focusNode': null,
-                    'focusOffset': 0,
-                    'isCollapsed': true,
-                    'rangeCount': 0,
-                    'type': "None"
-                };
-            },
-            () => function() { }
-        );
+        const origSelection = document.getSelection;
         setGetSet(document, 'getSelection', () => function() { console.log("tried document getselection"); }, () => function() { });
 
         // Just in case they try more in the future, we can try this
         //delete window.selection;
-        //setGetSet(window, 'onselectionchange', () => function() { console.log("onselectionchange"); }, () => function() {});
-        //setGetSet(document, 'onselectstart', () => function() { console.log("tried onselectstart"); }, () => function() {});
-        //setGetSet(document, 'createRange', () => function() { console.log("tried to create selection range"); }, () => function() { });
+        setGetSet(window, 'onselectionchange', () => function() { console.log("onselectionchange"); }, () => function() {});
+        setGetSet(document, 'onselectstart', () => function() { console.log("tried onselectstart"); }, () => function() {});
+        setGetSet(document, 'createRange', () => function() { console.log("tried to create selection range"); }, () => function() { });
+
+        var found = false;
+        function findMail(parent) {
+            const messages = parent.querySelectorAll("[data-legacy-message-id]");
+            for (var i=0; i<messages.length; i++) {
+                if (messages[i].innerText.indexOf('You do not have the option to forward') === -1) {
+                    continue;
+                }
+                console.log('message: ' + messages[i].innerText);
+                //chrome.runtime.sendMessage({content: messages[i].innerHTML}, function(response) {
+                found = true;
+                return;
+            }
+        }
 
         setTimeout(function() {
             addStyleString(".div { user-select: auto !important; }");
             addStyleString(".p { user-select: auto !important; }");
             addStyleString(".an { user-select: auto !important; }");
+            findMail(document);
 
-            //const messages = document.querySelectorAll("[data-legacy-message-id]");
-            //for (var i=0; i<messages.length; i++) {
-            //    if (messages[i].innerText.indexOf('You do not have the option to forward, copy, print, or download this email') === -1) {
-            //        continue;
-            //    }
-            //    chrome.runtime.sendMessage({content: messages[i].innerHTML}, function(response) {
-            //}
         }, 100);
+
+        function doCopy(event) {
+            const selection = origSelection.apply(document);
+            event.clipboardData.setData('text/plain', selection.toString());
+            event.preventDefault();
+        }
+
+        var observer = new MutationObserver(function(mutationList) {
+            for (var mutation of mutationList) {
+                switch(mutation.type) {
+                    case "attributes":
+                        mutation.target.oncopy = doCopy;
+                        break;
+                }
+                for (var child of mutation.addedNodes) {
+                    child.oncopy = doCopy;
+
+                    if (!found && child.getAttribute && child.getAttribute('cellpadding')) {
+                        findMail(child);
+                    }
+                }
+            }
+        });
+        observer.observe(document, {childList: true, subtree: true, attributes: true, attributeFilter: ['oncopy', 'cellpadding', 'dataset', 'data-message-id', 'message-id']});//, 'tabindex']});
     }
 
 } + ')();' ;
